1667fe73fe99d54690324abd81be2e6748b0932b
Otto Cline <185109514+Otto-Cline@users.noreply.github.com>
2026-08-02 12:51:44 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+
+# ["he", "wo"]
+    def encode(self, strs: List[str]) -> str:
+        s = ""
+        for word in strs:
+            s += str(len(word)) + "#" + word
+        print(s)
+        return s
+        
+# "2#he2#wo"
+    def decode(self, s: str) -> List[str]:
+        array = []
+        i=0
+        while i < len(s): #8
+#
+            num_of_chars = ""
+            while s[i] != "#":
+                num_of_chars += str(s[i])
+                i+= 1
+            num_of_chars = int(num_of_chars)
+
+            j=0
+            word = ""
+            while j < num_of_chars: #j < 2
+                j+=1
+                i+=1
+                word += s[i]
+            i+=1
+            array.append(word)
+            word = ""
+        return array
+
+    
